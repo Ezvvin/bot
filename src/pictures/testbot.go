21d72fa3536a9753a2d906675e20ce6a74c07300
Ezvvin <pavel.klimov2012@ya.ru>
2022-06-22 23:13:23 +0300

    ебола с картинками

---
+++ src/pictures/testbot.go
@@ -0,0 +1,74 @@
+package pictures
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func getBot(t *testing.T) (*tgbotapi.BotAPI, error) {
+	bot, err := tgbotapi.NewBotAPI("5437936243:AAG8qSRApD7V90ZZUVDM4ze0bcRbUC1rbrE")
+	bot.Debug = true
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	return bot, err
+}
+
+func TestGetUpdates(t *testing.T) {
+	bot, _ := getBot(t)
+
+	u := tgbotapi.NewUpdate(0)
+
+	_, err := bot.GetUpdates(u)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestSendWithNewPhoto(t *testing.T) {
+	bot, _ := getBot(t)
+
+	msg := tgbotapi.NewPhoto(5437936243, tgbotapi.FilePath("src/pictures/whitehoodie.jpg"))
+	msg.Caption = "Test"
+	_, err := bot.Send(msg)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestSendWithNewPhotoWithFileBytes(t *testing.T) {
+	bot, _ := getBot(t)
+
+	data, _ := ioutil.ReadFile("src/pictures/whitehoodie.jpg")
+	b := tgbotapi.FileBytes{Name: "src/pictures/whitehoodie.jpg", Bytes: data}
+
+	msg := tgbotapi.NewPhoto(5437936243, b)
+	msg.Caption = "Test"
+	_, err := bot.Send(msg)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestSendWithNewPhotoWithFileReader(t *testing.T) {
+	bot, _ := getBot(t)
+
+	f, _ := os.Open("src/pictures/whitehoodie.jpg")
+	reader := tgbotapi.FileReader{Name: "src/pictures/whitehoodie.jpg", Reader: f}
+
+	msg := tgbotapi.NewPhoto(5437936243, reader)
+	msg.Caption = "Test"
+	_, err := bot.Send(msg)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
--- src/sendpictures.go
+++ src/sendpictures.go
@@ -1,20 +1,28 @@
 package pictures
 
-import (
-	"io/ioutil"
+// import (
+// 	"image"
+// 	"io/ioutil"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-)
+// 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+// )
 
-func Sendpictur(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-	photoBytes, err := ioutil.ReadFile("bot/src/pictures/whitehoodie.png")
-	if err != nil {
-		panic(err)
-	}
-	photoFileBytes := tgbotapi.FileBytes{
-		Name:  "whitehoodie",
-		Bytes: photoBytes,
-	}
-	chatID := 12345678
-	Sendpictur, err := bot.Send(tgbotapi.NewPhotoUpload(int64(chatID), photoFileBytes))
-}
+// func Sendpictur(bot *tgbotapi.BotAPI, update tgbotapi.Update, message *tgbotapi.Message) {
+// 	image := tgbotapi.NewPhotoUpload(5437936243, "bot/src/pictures/whitehoodie.png")
+// 	_, err = bot.Send(image)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	photoBytes, err := ioutil.ReadFile("bot/src/pictures/whitehoodie.png")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	photoFileBytes := tgbotapi.FileBytes{
+// 		Name:  "whitehoodie",
+// 		Bytes: photoBytes,
+// 	}
+
+// 	chatID := 5437936243
+// 	Message, err := bot.Send(tgbotapi.NewPhotoUpload(int64(chatID), photoFileBytes))
+// }
