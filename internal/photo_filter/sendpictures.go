d80fd620fe10aed27f0f5226862ed667e6a88c9a
Ezvvin <pavel.klimov2012@ya.ru>
2022-06-24 01:21:00 +0300

    Bot9r

--- internal/photo_filter/sendpictures.go
+++ internal/photo_filter/sendpictures.go
@@ -1,28 +1,27 @@
 package photo_filter
 
-// import (
-// 	"image"
-// 	"io/ioutil"
+import (
+	// "io/ioutil"
 
-// 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-// )
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
-// func Sendpictur(bot *tgbotapi.BotAPI, update tgbotapi.Update, message *tgbotapi.Message) {
-// 	image := tgbotapi.NewPhotoUpload(5437936243, "bot/src/pictures/whitehoodie.png")
-// 	_, err = bot.Send(image)
+func Sendpictur(bot *tgbotapi.BotAPI, update tgbotapi.Update, message *tgbotapi.Message) {
+	image := tgbotapi.NewPhoto(5437936243, tgbotapi.FilePath("../../src/picturies/white_hoodie/whitehoodie.jpg"))
+	_, err := bot.Send(image)
 
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	photoBytes, err := ioutil.ReadFile("bot/src/pictures/whitehoodie.png")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	photoFileBytes := tgbotapi.FileBytes{
-// 		Name:  "whitehoodie",
-// 		Bytes: photoBytes,
-// 	}
+	if err != nil {
+		panic(err)
+	}
+	// photoBytes, err := ioutil.ReadFile("../../src/picturies/white_hoodie/whitehoodie.jpg")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// photoFileBytes := tgbotapi.FileBytes{
+	// 	Name:  "whitehoodie.jpg",
+	// 	Bytes: photoBytes,
+	// }
 
-// 	chatID := 5437936243
-// 	Message, err := bot.Send(tgbotapi.NewPhotoUpload(int64(chatID), photoFileBytes))
-// }
+	// chatID := 5437936243
+	// Message, err := bot.Send(tgbotapi.NewPhotoUpload(int64(chatID), photoFileBytes))
+}
--- internal/telegram_bot/command_impl/start.go
+++ internal/telegram_bot/command_impl/start.go
@@ -2,13 +2,14 @@ package commandimpl
 
 import (
 	"bot/internal/domain"
+	"fmt"
 	"log"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func Start(userMap map[int64]domain.Location, bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Добро пожаловать в наш магазин одежды  'LÚQ' 👋")
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("%s, Добро пожаловать в наш магазин одежды  'LÚQ' 👋", update.Message.From.UserName))
 	msg.ReplyMarkup = domain.MainMenuKeyboard
 	if _, err := bot.Send(msg); err != nil {
 		log.Panic(err)
