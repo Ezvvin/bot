0592d947e5beca748a2e577f2442b3bcd5c907c2
Ezvvin <pavel.klimov2012@ya.ru>
2022-06-23 20:04:25 +0300

    //

--- internal/photoFilter/testbot.go
+++ internal/photoFilter/testbot.go
@@ -1,74 +1,74 @@
 package photoFilter
 
-import (
-	"io/ioutil"
-	"os"
-	"testing"
+// import (
+// 	"io/ioutil"
+// 	"os"
+// 	"testing"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-)
+// 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+// )
 
-func getBot(t *testing.T) (*tgbotapi.BotAPI, error) {
-	bot, err := tgbotapi.NewBotAPI("")
-	bot.Debug = true
+// func getBot(t *testing.T) (*tgbotapi.BotAPI, error) {
+// 	bot, err := tgbotapi.NewBotAPI("")
+// 	bot.Debug = true
 
-	if err != nil {
-		t.Error(err)
-	}
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
 
-	return bot, err
-}
+// 	return bot, err
+// }
 
-func TestGetUpdates(t *testing.T) {
-	bot, _ := getBot(t)
+// func TestGetUpdates(t *testing.T) {
+// 	bot, _ := getBot(t)
 
-	u := tgbotapi.NewUpdate(0)
+// 	u := tgbotapi.NewUpdate(0)
 
-	_, err := bot.GetUpdates(u)
+// 	_, err := bot.GetUpdates(u)
 
-	if err != nil {
-		t.Error(err)
-	}
-}
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// }
 
-func TestSendWithNewPhoto(t *testing.T) {
-	bot, _ := getBot(t)
+// func TestSendWithNewPhoto(t *testing.T) {
+// 	bot, _ := getBot(t)
 
-	msg := tgbotapi.NewPhoto(5437936243, tgbotapi.FilePath("src/pictures/whitehoodie.jpg"))
-	msg.Caption = "Test"
-	_, err := bot.Send(msg)
+// 	msg := tgbotapi.NewPhoto(5437936243, tgbotapi.FilePath("src/pictures/whitehoodie.jpg"))
+// 	msg.Caption = "Test"
+// 	_, err := bot.Send(msg)
 
-	if err != nil {
-		t.Error(err)
-	}
-}
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// }
 
-func TestSendWithNewPhotoWithFileBytes(t *testing.T) {
-	bot, _ := getBot(t)
+// func TestSendWithNewPhotoWithFileBytes(t *testing.T) {
+// 	bot, _ := getBot(t)
 
-	data, _ := ioutil.ReadFile("src/pictures/whitehoodie.jpg")
-	b := tgbotapi.FileBytes{Name: "src/pictures/whitehoodie.jpg", Bytes: data}
+// 	data, _ := ioutil.ReadFile("src/pictures/whitehoodie.jpg")
+// 	b := tgbotapi.FileBytes{Name: "src/pictures/whitehoodie.jpg", Bytes: data}
 
-	msg := tgbotapi.NewPhoto(5437936243, b)
-	msg.Caption = "Test"
-	_, err := bot.Send(msg)
+// 	msg := tgbotapi.NewPhoto(5437936243, b)
+// 	msg.Caption = "Test"
+// 	_, err := bot.Send(msg)
 
-	if err != nil {
-		t.Error(err)
-	}
-}
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// }
 
-func TestSendWithNewPhotoWithFileReader(t *testing.T) {
-	bot, _ := getBot(t)
+// func TestSendWithNewPhotoWithFileReader(t *testing.T) {
+// 	bot, _ := getBot(t)
 
-	f, _ := os.Open("src/pictures/whitehoodie.jpg")
-	reader := tgbotapi.FileReader{Name: "src/pictures/whitehoodie.jpg", Reader: f}
+// 	f, _ := os.Open("src/pictures/whitehoodie.jpg")
+// 	reader := tgbotapi.FileReader{Name: "src/pictures/whitehoodie.jpg", Reader: f}
 
-	msg := tgbotapi.NewPhoto(5437936243, reader)
-	msg.Caption = "Test"
-	_, err := bot.Send(msg)
+// 	msg := tgbotapi.NewPhoto(5437936243, reader)
+// 	msg.Caption = "Test"
+// 	_, err := bot.Send(msg)
 
-	if err != nil {
-		t.Error(err)
-	}
-}
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
+// }
