30785d84af30242434db2fe205f9a142a14c446d
Vyacheslav Labetsky <labi-98@mail.ru>
2022-06-24 15:46:46 +0300

    Fix commends

--- internal/telegram_bot/command_impl/сommends.go
+++ internal/telegram_bot/command_impl/сommends.go
@@ -12,8 +12,12 @@ func Commends(userMap map[int64]domain.Location, bot *tgbotapi.BotAPI, update tg
 	url := "https://vk.com/topic-200472710_49047107"
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Вы можете прочитать или оставить отзыв о товаре в нашей группе Вконтакте: [VKontakte](%s)", url))
 	msg.ReplyMarkup = domain.CommendsKeyboard
+	// Меняю мод парсера для гипперссылок
 	msg.ParseMode = "MarkdownV2"
+	// Отключаю превью страницы
+	msg.DisableWebPagePreview = true
 	if _, err := bot.Send(msg); err != nil {
+		// TODO: Сделай через нормальный логгер
 		log.Panic(err)
 	}
 	userMap[update.Message.From.ID] = domain.Location_Commends
