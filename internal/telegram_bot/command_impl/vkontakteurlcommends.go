99e828aabe00d58795e19594e20474ba8e3290e8
Ezvvin <pavel.klimov2012@ya.ru>
2022-06-28 16:07:31 +0300

    vkbottom

--- internal/telegram_bot/command_impl/vkontakteurlcommends.go
+++ internal/telegram_bot/command_impl/vkontakteurlcommends.go
@@ -10,11 +10,11 @@ import (
 
 func VkontakteurlCommends(userMap map[int64]domain.Location, bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 	url := "https://vk.com/topic-200472710_49047107"
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "url")
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, url)
 	msg.ReplyMarkup = domain.CommendsKeyboard
-	msg.Entities = []tgbotapi.MessageEntity{tgbotapi.MessageEntity{URL: url, Type: "url"}}
+	msg.Entities = []tgbotapi.MessageEntity{tgbotapi.MessageEntity{Type: "url"}}
 	if _, err := bot.Send(msg); err != nil {
-		//логгер
+		
 		log.WithError(err).Errorf(domain.ErrCommand_Init.Error(), "vkcommendURL")
 	}
 	userMap[update.Message.From.ID] = domain.Location_Commends
