f0392040c0d6492382750764b52ff98ec4390fa4
Vyacheslav <59142155+ImWillow@users.noreply.github.com>
2022-07-04 02:56:55 +0300

    Добавил коммент

--- internal/domain/inlinekeyboards.go
+++ internal/domain/inlinekeyboards.go
@@ -3,6 +3,7 @@ package domain
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
+// TODO: Переименуй файл "inline_keyboards"
 // инлайн кнопки контактов
 var ContactsMenuKeyboard = tgbotapi.NewInlineKeyboardMarkup(
 	tgbotapi.NewInlineKeyboardRow(
