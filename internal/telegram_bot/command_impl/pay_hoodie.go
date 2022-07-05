f07398964f35e4fb665a252f68e1e1ed84598804
Ezvvin <pavel.klimov2012@ya.ru>
2022-07-05 12:59:09 +0300

    fix

--- internal/telegram_bot/command_impl/pay_hoodie.go
+++ internal/telegram_bot/command_impl/pay_hoodie.go
@@ -10,11 +10,11 @@ import (
 
 func PayHoodie(userMap map[int64]domain.Location, bot *tgbotapi.BotAPI, update tgbotapi.Update) {
 
-	msg := tgbotapi.NewInvoice(update.Message.Chat.ID, "ЗАГОЛОВОК", "описание", "загрузка оплаты", "TOKEN", "старт параметры", "USD", []tgbotapi.LabeledPrice{{Label: "USD", Amount: 200}})
+	msg := tgbotapi.NewInvoice(update.Message.Chat.ID, "ЗАГОЛОВОК", "описание", "загрузка оплаты", "TOKEN", "старт параметры", "RUB", []tgbotapi.LabeledPrice{{Label: "RUB", Amount: 3500}})
 	msg.NeedName = true
 	msg.NeedShippingAddress = true
 	msg.NeedPhoneNumber = true
-	msg.SuggestedTipAmounts = []int{3750, 37508026}
+	msg.SuggestedTipAmounts = []int{3500, 4000}
 	msg.MaxTipAmount = 37508026
 	if _, err := bot.Send(msg); err != nil {
 		log.WithError(err).Errorf(domain.ErrCommand_Init.Error(), "pay hoodie")
