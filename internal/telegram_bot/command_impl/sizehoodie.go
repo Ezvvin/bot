fd48937188690a65d8e5423540fd319a01d2a62a
Ezvvin <pavel.klimov2012@ya.ru>
2022-07-08 16:46:51 +0300

    5

--- internal/telegram_bot/command_impl/sizehoodie.go
+++ internal/telegram_bot/command_impl/sizehoodie.go
@@ -8,12 +8,12 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func Delivery(userMap map[int64]domain.Location, bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Выберите тип доставки:")
-	msg.ReplyMarkup = domain.DeliveryKeyboard
+func SizeHoodie(userMap map[int64]domain.Location, bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Выберите нужный вам размер:")
+	msg.ReplyMarkup = domain.SizeHoodieKeyboard
 	if _, err := bot.Send(msg); err != nil {
-		log.WithError(err).Errorf(domain.ErrCommand_Init.Error(), "deliverybutton")
+		log.WithError(err).Errorf(domain.ErrCommand_Init.Error(), "Sizehoodiebutton")
 	}
-	userMap[update.Message.From.ID] = domain.Location_Delivery
+	userMap[update.Message.From.ID] = domain.Location_SizeHoodie
 
 }
